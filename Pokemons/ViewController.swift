2ea884977feac3d7e53b3d0acfb51713c4b3ac86
Ronald Hernandez <2ronald.hernandez@gmail.com>
2017-03-13 06:25:39 -0500

    Initial Commit

---
+++ Pokemons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemons
+//
+//  Created by Ronald Hernandez on 3/13/17.
+//  Copyright © 2017 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
